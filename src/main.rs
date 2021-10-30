d50160eef61185b14fdf58c155024317f2067d72
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-10-30 20:32:41 +0100

    Formatting!

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,13 @@ use embedded_time::rate::*;
 use git_version::git_version;
 use hal::clocks::Clock;
 use panic_probe as _;
-use pico::{self, hal::{self, pac, pio::{self as hal_pio, PIOExt}}};
+use pico::{
+	self,
+	hal::{
+		self, pac,
+		pio::{self as hal_pio, PIOExt},
+	},
+};
 
 // -----------------------------------------------------------------------------
 // Static and Const Data
@@ -140,7 +146,6 @@ fn main() -> ! {
 	let _blue2 = pins.gpio12.into_mode::<hal::gpio::FunctionPio0>();
 	let _blue3 = pins.gpio13.into_mode::<hal::gpio::FunctionPio0>();
 
-
 	// Need to configure PIO0SM0 to run the timing loop. We post timing data
 	// and it busy-waits the appropriate amount of time and trigger the
 	// appropriate interrupts.
@@ -155,7 +160,8 @@ fn main() -> ! {
 
 	let (mut pio, sm0, _sm1, _sm2, _sm3) = pac.PIO0.split(&mut pac.RESETS);
 
-	let program = pio_proc::pio!(32,
+	let program = pio_proc::pio!(
+		32,
 		"
 		; Block until FIFO receives data
 		pull block
@@ -168,7 +174,8 @@ fn main() -> ! {
 		loop0:
 		jmp x-- loop0 ; Spin until X is zero
 		.wrap
-		");
+		"
+	);
 
 	let installed = pio.install(&program.program).unwrap();
 	let (mut sm, _rx_fifo, mut tx_fifo) = hal_pio::PIOBuilder::from_program(installed)
@@ -180,7 +187,7 @@ fn main() -> ! {
 		.out_shift_direction(hal_pio::ShiftDirection::Right)
 		.pull_threshold(32)
 		.build(sm0);
-	sm.set_pindirs([(0, hal_pio::PinDir::Output),(1, hal_pio::PinDir::Output)]);
+	sm.set_pindirs([(0, hal_pio::PinDir::Output), (1, hal_pio::PinDir::Output)]);
 
 	sm.start();
 
@@ -197,10 +204,10 @@ fn main() -> ! {
 		// This should all be moved off into an IRQ so it doesn't block the main thread
 		for scanline in 1..=525 {
 			// 640 x 480, negative h-sync - four periods per video line
-	
-			let need_vsync = scanline >= 490 && scanline <= 491; 
+
+			let need_vsync = scanline >= 490 && scanline <= 491;
 			let _is_visible = scanline <= 480;
-		
+
 			// This if the front porch
 			load_timing(&mut tx_fifo, 16, true, need_vsync, false);
 			// This is the sync pulse
@@ -213,14 +220,28 @@ fn main() -> ! {
 	}
 }
 
-fn load_timing(fifo: &mut hal_pio::Tx<(pac::PIO0, hal_pio::SM0)>, period: u16, hsync: bool, vsync: bool, want_irq: bool) {
+fn load_timing(
+	fifo: &mut hal_pio::Tx<(pac::PIO0, hal_pio::SM0)>,
+	period: u16,
+	hsync: bool,
+	vsync: bool,
+	want_irq: bool,
+) {
 	let command = if want_irq {
 		// This command sets IRQ 0
-		pio::InstructionOperands::IRQ { clear: false, wait: false, index: 0, relative: false }
-	}
-	else {
+		pio::InstructionOperands::IRQ {
+			clear: false,
+			wait: false,
+			index: 0,
+			relative: false,
+		}
+	} else {
 		// This command is a no-op (it moves Y into Y)
-		pio::InstructionOperands::MOV { destination: pio::MovDestination::Y, op: pio::MovOperation::None, source: pio::MovSource::Y }
+		pio::InstructionOperands::MOV {
+			destination: pio::MovDestination::Y,
+			op: pio::MovOperation::None,
+			source: pio::MovSource::Y,
+		}
 	};
 	let mut value: u32 = u32::from(command.encode());
 	if hsync {
