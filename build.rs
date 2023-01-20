baee3e2121d528ac5b41471b1c80f13a9dd1022f
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-01-20 17:16:10 +0000

    Copying build steps from Pico BMC.

--- build.rs
+++ build.rs
@@ -32,9 +32,15 @@ fn main() {
 	// Generate a file containing the firmware version
 	let version_output = std::process::Command::new("git")
 		.current_dir(env::var_os("CARGO_MANIFEST_DIR").unwrap())
-		.args(["describe", "--long", "--dirty"])
+		.args(["describe", "--tags", "--dirty"])
 		.output()
 		.expect("running git-describe");
+
+	println!(
+		"Version is {:?}",
+		std::str::from_utf8(&version_output.stdout)
+	);
+	println!("Error is {:?}", std::str::from_utf8(&version_output.stderr));
 	assert!(version_output.status.success());
 
 	// Remove the trailing newline
